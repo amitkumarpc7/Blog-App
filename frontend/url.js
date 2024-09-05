d309d55bbd39e968813687ec258b5bf4c646e551
amitkumarpc7 <amitkpc11@gmail.com>
2024-09-05 13:07:14 +0530

    updated index.js

--- frontend/url.js
+++ frontend/url.js
@@ -1,2 +1,4 @@
-export const URL=import.meta.env.VITE_URL
-export const IF=import.meta.env.VITE_IF
+// export const URL=import.meta.env.VITE_URL
+// export const IF=import.meta.env.VITE_IF
+export const URL="https://blog-app-backend-xcuj.onrender.com"
+export const IF="https://blog-app-backend-xcuj.onrender.com/images/"
