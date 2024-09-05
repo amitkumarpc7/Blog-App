71d2fb64428d35a92ddc3ce435680a985be7c4c4
amitkumarpc7 <amitkpc11@gmail.com>
2024-09-05 12:56:25 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,11 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images", express.static("images"));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: ["http://localhost:5173", "https://blog-app-22b8.onrender.com"],
+  credentials: true
+}));
 // Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
